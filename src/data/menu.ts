611dc401d75339824e4fc90c64340625554d705f
Sheran21 <samarejanula@gmail.com>
2026-01-17 14:15:52 +0530

    aded new items, changed names and prices

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,3 +1,9 @@
+import roastPaanImg from '../assets/Menu-Images/Roast-Paan.jpeg';
+import ChiKottuImg from '../assets/Menu-Images/Kottu-Chicken.jpeg';
+import CheeseKottuImg from '../assets/Menu-Images/CheeseKottu-Chicken.jpeg';
+import FriedRiceImg from '../assets/Menu-Images/FriedRice-Sea.jpeg';
+import GarlicRiceImg from '../assets/Menu-Images/Garlic-Rice.jpeg';
+
 export interface MenuItem {
   label: string;
   price: number;
@@ -7,53 +13,60 @@ export interface MenuCategory {
   category: string;
   items: MenuItem[];
   isSpecial?: boolean;
+  imageUrl?: string;
 }
 
 export const MENU_DATA: MenuCategory[] = [
   {
     category: "ROAST PAAN KOTTU",
-    items: [{ label: "POK", price: 900 }],
+    imageUrl: roastPaanImg,
+    items: [{ label: "PORK", price: 800 }],
+    isSpecial: true
+  },
+  {
+    category: "GARLIC RICE Combo",
+    imageUrl: GarlicRiceImg,
+    items: [{ label: "GARLIC RICE PORK & 1 FREE EGB(250ML)", price: 1200 }],
     isSpecial: true
   },
   {
     category: "KOTTU",
+    imageUrl: ChiKottuImg,
     items: [
       { label: "VEG", price: 400 },
       { label: "EGG", price: 400 },
-      { label: "CHI", price: 500 },
-      { label: "POK", price: 900 }
+      { label: "CHICKEN", price: 500 },
+      { label: "PORK", price: 900 }
     ]
   },
   {
     category: "CHEESE KOTTU",
+    imageUrl: CheeseKottuImg,
     items: [
       { label: "VEG", price: 800 },
       { label: "EGG", price: 800 },
-      { label: "CHI", price: 900 }
+      { label: "CHICKEN", price: 900 }
     ]
   },
   {
     category: "FRIED RICE",
+    imageUrl: FriedRiceImg,
     items: [
       { label: "VEG", price: 400 },
       { label: "EGG", price: 400 },
-      { label: "CHI", price: 500 },
+      { label: "CHICKEN", price: 500 },
       { label: "SEA", price: 800 }
     ]
   },
   {
-    category: "GARLIC RICE",
-    items: [{ label: "POK", price: 1300 }]
-  },
-  {
     category: "ADD ON",
     items: [
-      { label: "CH LG", price: 800 },
-      { label: "JU SA", price: 200 },
+      { label: "CHICKEN WHOLE LEG", price: 800 },
+      { label: "JUMBO SAUSAGE", price: 200 },
       { label: "EGG", price: 100 }
     ]
   }
 ];
 
-export const MENU_TITLE = "Buddy Potion Meals Menu";
+export const MENU_TITLE = "Buddy Portion Meals Menu";
 export const MENU_SUBTITLE = "EGB Kema Kalawa • Galle Face 2026";
